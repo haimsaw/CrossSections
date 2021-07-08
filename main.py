1800238ddbb06b162a6f29f14c868fbb1ba94a38
Haim Sawdayee <haimsaw@gmail.com>
2021-07-09 02:44:03 +0300

    centralize shape before rendering

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ class ConnectedComponent:
 		component = iter(next(csl_file).strip().split(" "))  # todo holes
 		sizes = next(component).split("h") + [0]
 
-		self.n_vertices_in_component, self.n_holes = int(sizes[0]), int(sizes[1])  # todo what is n_holes?
+		n_vertices_in_component, self.n_holes = int(sizes[0]), int(sizes[1])  # todo what is n_holes?
 		component = map(int, component)
 		self.label = next(component)
 		# self.label = 1 if self.n_holes> 0 else 0
 		self.vertices_in_component = list(component)
-		assert len(self.vertices_in_component) == self.n_vertices_in_component
+		assert len(self.vertices_in_component) == n_vertices_in_component
 
 
 class Plane:
@@ -48,6 +48,13 @@ class Plane:
 	@classmethod
 	def empty_plane(cls, plane_id, plane_params):
 		return cls(plane_id, plane_params, np.array([]), [])
+
+	def __isub__(self, other: np.array):
+		assert len(other) == 3
+		self.vertices -= other
+		new_D = self.plane_params[3] + np.dot(self.plane_params[:3], other)  # normal*(x-x_0)=0
+		self.plane_params = self.plane_params[:3]+(new_D,)
+		return self
 
 
 class CSL:
@@ -84,6 +91,11 @@ class CSL:
 		plane_id = len(self.planes) + 1
 		self.planes.append(Plane.empty_plane(plane_id, plane_params))
 
+	def centralize(self):
+		mean = np.mean(self.all_vertices, axis=0)
+		for plane in self.planes:
+			plane -= mean
+
 	def add_boundary_planes(self, margin_percent):
 		top, bottom = self. vertices_boundaries
 		margin = margin_percent * (top - bottom)
@@ -103,7 +115,7 @@ class CSL:
 
 
 class Renderer:
-	def __init__(self, csl):
+	def __init__(self, csl, box):
 		self.csl = csl
 
 		self.zoom = 1
@@ -115,9 +127,10 @@ class Renderer:
 
 		self.colors = [[random(), random(), random()] for _ in range(self.csl.n_labels + 1)]
 		self.scale_factor = self.csl.scale_factor
+		self.box = box / self.scale_factor
 
 		glfw.init()
-		self.window = glfw.create_window(800, 600, "Cross Sections", None, None)
+		self.window = glfw.create_window(1600, 1200, "Cross Sections", None, None)
 		glfw.set_window_pos(self.window, 400, 200)
 		glfw.make_context_current(self.window)
 
@@ -138,15 +151,16 @@ class Renderer:
 			for connected_component in plane.connected_components:
 				vertices = plane.vertices[connected_component.vertices_in_component]
 				vertices /= self.scale_factor
+				self.draw_vertices(vertices, connected_component.label)
+		self.draw_vertices(self.box, self.csl.n_labels )
 
-				v = np.array(vertices.flatten(), dtype=np.float32)
-				glVertexPointer(3, GL_FLOAT, 0, v)
-
-				color = self.colors[connected_component.label] * len(vertices)
-				color = np.array(color, dtype=np.float32)
-				glColorPointer(3, GL_FLOAT, 0, color)
-
-				glDrawArrays(GL_LINE_LOOP, 0, len(vertices))
+	def draw_vertices(self, vertices, label):
+		v = np.array(vertices.flatten(), dtype=np.float32)
+		glVertexPointer(3, GL_FLOAT, 0, v)
+		color = self.colors[label] * len(vertices)
+		color = np.array(color, dtype=np.float32)
+		glColorPointer(3, GL_FLOAT, 0, color)
+		glDrawArrays(GL_LINE_LOOP, 0, len(vertices))
 
 	def event_loop(self):
 		origin_x = 0
@@ -155,24 +169,7 @@ class Renderer:
 		while not glfw.window_should_close(self.window):
 			glfw.poll_events()
 			glClear(GL_COLOR_BUFFER_BIT)
-			glLoadIdentity()
-
-			x, y = glfw.get_cursor_pos(self.window)
-
-			if glfw.get_mouse_button(self.window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
-				self.rho -= (origin_x - x) / 10
-				self.theta -= (origin_y - y) / 10
-
-			elif glfw.get_mouse_button(self.window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS:
-				self.dx -= (origin_x - x) / 100
-				self.dy += (origin_y - y) / 100
-			glTranslate(self.dx, self.dy, 0)
-
-			glRotatef(self.rho, 0.0, 1.0, 0.0)
-			glRotatef(self.theta, 1.0, 0.0, 0.0)  # todo axis of rotation
-
-			glScalef(self.zoom, self.zoom, self.zoom)
-
+			x, y = self.handle_transformations(origin_x, origin_y)
 
 			self.draw_scene()
 			glfw.swap_buffers(self.window)
@@ -182,22 +179,44 @@ class Renderer:
 
 		glfw.terminate()
 
+	def handle_transformations(self, origin_x, origin_y):
+		glLoadIdentity()
+		x, y = glfw.get_cursor_pos(self.window)
+		if glfw.get_mouse_button(self.window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
+			self.rho -= (origin_x - x) / 10
+			self.theta -= (origin_y - y) / 10
+
+		elif glfw.get_mouse_button(self.window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS:
+			self.dx -= (origin_x - x) / 200
+			self.dy += (origin_y - y) / 200
+
+		glTranslate(self.dx, self.dy, 0)
+
+		print(f'rho={self.rho:.3f} theta={self.theta:.3f}')
+
+		glRotatef(self.rho, 0.0, 1.0, 0.0)
+		glRotatef(self.theta,  math.cos(math.radians(self.rho)), 0.0, math.sin(math.radians(self.rho)))  # todo axis of rotation
+
+		glScalef(self.zoom, self.zoom, self.zoom)
+		return x, y
+
 
 def main():
 
-	# csl = CSL("csl-files/Heart-25-even-better.csl")
+	csl = CSL("csl-files/Heart-25-even-better.csl")
 	# csl = CSL("csl-files/Horsers.csl")
 	# csl = CSL("csl-files/Brain.csl")
-	#csl = CSL("csl-files/Abdomen.csl")
+	# csl = CSL("csl-files/Abdomen.csl")
 	# csl = CSL("csl-files/Vetebrae.csl")
-	csl = CSL("csl-files/rocker-arm.csl")
+	# csl = CSL("csl-files/rocker-arm.csl")
 	# csl = CSL("csl-files/SideBishop.csl")
 	# csl = CSL("csl-files/ParallelEight.csl")
 	# csl = CSL("csl-files/ParallelEightMore.csl")
 
+	csl.centralize()
 	box = csl.add_boundary_planes(0.2)
 
-	renderer = Renderer(csl)
+	renderer = Renderer(csl, box)
 	renderer.event_loop()
 
 
