430627ac1a536bb93e74fda409ac62544cd4918c
Haim Sawdayee <haimsaw@gmail.com>
2022-02-15 13:47:22 +0200

    learn sdf WIP

--- BoundaryResterizer.py
+++ BoundaryResterizer.py
@@ -1,30 +1,43 @@
 from torch.utils.data import Dataset
 import numpy as np
 
+
 class RasterizedCslBoundaryDataset(Dataset):
     def __init__(self, csl, n_samples_per_edge, transform=None, target_transform=None):
         self.csl = csl
 
-        # todo n_samples_per_edge should be per plane?
+        self.xyzs = np.empty(shape=(0, 3))
+        self.normals = np.empty(shape=(0, 3))
 
         for plane in csl.planes:
             if not plane.is_empty:
                 for connected_component in plane.connected_components:
                     verts = plane.vertices[connected_component.vertices_indices]
                     verts1 = verts[1:] + verts[0:1]
+
+                    # no need to invert order for holes since they ordered cw
                     normals = np.cross(verts1 - verts, plane.normal)
 
                     for p0, p1, normal in zip(verts, verts1, normals):
-                        pass
+                        normal /= np.linalg.norm(normal)
 
+                        # todo n_samples_per_edge should be per plane?
+                        self.xyzs = np.concatenate((self.xyzs, np.linspace(p0, p1, n_samples_per_edge, endpoint=False).T))
+                        self.normals = np.concatenate((self.normals, np.repeat([normal], n_samples_per_edge, axis=0)))
 
         self.transform = transform
-        self.target_transform = target_transform
+        self.normal_transform = target_transform
 
     def len(self):
-        pass
+        return len(self.xyzs)
 
     def __getitem__(self, idx):
-        pass
-        # return xyz, normal
+        xyz = self.xyzs[idx]
+        normal = self.normals[idx]
 
+        if self.transform:
+            xyz = self.transform(xyz)
+        if self.normal_transform:
+            normal = self.normal_transform(normal)
+
+        return xyz, normal
