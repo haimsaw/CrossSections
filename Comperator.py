9affc400ccaaeaebea69d37e9d3744a942d031d9
hasawday <haimsaw@gmail.com>
2022-05-08 18:19:14 +0300

    bug fix

--- Comperator.py
+++ Comperator.py
@@ -7,13 +7,14 @@ def hausdorff_distance(original_mesh_path, recon_mesh_path, save_path):
     ms = pymeshlab.MeshSet()
     ms.load_new_mesh(original_mesh_path)
     ms.load_new_mesh(recon_mesh_path)
-    res = ms.get_hausdorff_distance(sampledmesh=0, targetmesh=1, sampleedge=True)
+    res1 = ms.get_hausdorff_distance(sampledmesh=0, targetmesh=1, sampleedge=True)
+    res2 = ms.get_hausdorff_distance(sampledmesh=1, targetmesh=0, sampleedge=True)
     print(f'original={original_mesh_path}, recon={recon_mesh_path}')
-    print(f'hausdorff_distance={res}')
+    print(f'\nhausdorff:\n{res1}\n{res2}\ndist={max(res1["max"], res2["max"])}')
     if save_path is not None:
         with open(f'{save_path}/hausdorff_distance.json', 'w') as fp:
-            json.dump(res, fp, indent=4)
-    return res
+            json.dump(res1, fp, indent=4)
+            json.dump(res2, fp, indent=4)
 
 
 if __name__ == '__main__':
