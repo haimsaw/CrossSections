cd278ccf882fbf0f62818c1dff7adc2d8b0f667e
hasawday <haimsaw@gmail.com>
2022-05-12 12:53:11 +0300

    add csl_to_xyz.py

--- csl_to_xyz.py
+++ csl_to_xyz.py
@@ -10,15 +10,15 @@ def csl_to_xyz():
 
     csl = get_csl(hp.bounding_planes_margin, save_path)
     pts = []
-    for plane in csl.planes():
+    for plane in csl.planes:
         for cc in plane.connected_components:
             verts = plane.vertices[cc.vertices_indices]
             verts2 = np.concatenate((verts[1:], verts[0:1]))
             for e1, e2  in zip(verts, verts2):
-                pts += [e1*d + e2(1-d) for d in np.linspace(0, 1, n_points_per_edge)]
+                pts += [e1*d + e2*(1-d) for d in np.linspace(0, 1, n_points_per_edge)]
 
     pts = np.array(pts)
-    ax = plt.axes()
+    ax = plt.axes(projection='3d')
     ax.scatter(*pts.T, color='red')
     plt.show()
 
