c45af8d74645ee60d5a1d9b1cfe09c1e043a423e
hasawday <haimsaw@gmail.com>
2022-07-18 17:04:07 +0300

    use argparse

--- Helpers.py
+++ Helpers.py
@@ -32,7 +32,7 @@ def timing(f):
         ts = time()
         result = f(*args, **kw)
         te = time()
-        print(f'func:{f.__name__} args:[{args}, {kw}] took: {te-ts:2.4f} sec')
+        print(f'func:{f.__name__} took: {te-ts:2.4f} sec')
         return result
     return wrap
 
