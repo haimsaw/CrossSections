0b410e5218b44f21597667f47cb870aff0af48ae
hasawday <haimsaw@gmail.com>
2022-07-18 15:18:01 +0300

    use argparse

--- hp.py
+++ hp.py
@@ -5,18 +5,36 @@ from sampling.CSL import CSL
 from sampling.Slicer import make_csl_from_mesh
 from sampling.csl_to_xyz import csl_to_xyz
 
+import argparse
 
 def get_csl(bounding_planes_margin, save_path, name):
-    csl = CSL.from_csl_file(f"./data/csl-files/{name}.csl")
+    # csl = CSL.from_csl_file(f"./data/csl-files/{name}.csl")
 
     # csl = CSL.from_csl_file(f"./data/csl_from_mesh/{name}_from_mesh.csl")
 
-    # csl = make_csl_from_mesh('./data/eight.obj', save_path)
-    # csl = make_csl_from_mesh('data/obj/armadillo.obj', save_path)
-    # csl = make_csl_from_mesh('data/obj/lamp004_fixed.obj', save_path)
+    csl = make_csl_from_mesh(f'./data/{name}.obj', save_path)
 
     csl.adjust_csl(bounding_planes_margin=bounding_planes_margin)
     return csl
+
+
+parser = argparse.ArgumentParser(description='Run NeRP.')
+
+parser.add_argument('gpu', type=int, default=0, help='an integer for the accumulator')
+
+
+# sampling
+
+# resolutions
+
+# architecture
+parser.add_argument('n_pe', type=int, default=4, dest='num_embedding_freqs', help='number of embedding freqs')
+parser.add_argument('hss', type=int, default=32, dest='hidden_state_size', help='hidden state size')
+parser.add_argument('n_loops', type=int, default=10, dest='n_loops', help='n of times we iterate')
+
+# training
+
+args = parser.parse_args()
 
 
 class HP:
@@ -34,12 +52,10 @@ class HP:
         self.intermediate_sampling_resolution_3d = (64, 64, 64)
 
         # architecture
-        self.num_embedding_freqs = 4
         self.spherical_coordinates = False
         self.hidden_layers = [64]*5
         self.hidden_state_size = 32
         self.hidden_state_embedder = True
-        self.n_loops = 10
 
         # loss
         self.density_lambda = 1
